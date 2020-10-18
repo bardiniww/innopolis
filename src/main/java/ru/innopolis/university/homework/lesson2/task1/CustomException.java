171e6cf4eef5749d7b529b39c4ca0f56c850ce76
Ivan Bardin <bardiniww@gmail.com>
2020-10-18 12:09:00 +0300

    npe and custom exeptions added

---
+++ src/main/java/ru/innopolis/university/homework/lesson2/task1/CustomException.java
@@ -0,0 +1,11 @@
+package ru.innopolis.university.homework.lesson2.task1;
+
+class CustomException {
+    public static void main(String... args) {
+        req();
+    }
+
+    private static void req() {
+        req();
+    }
+}
---
+++ src/main/java/ru/innopolis/university/homework/lesson2/task1/NullPointerException.java
@@ -0,0 +1,8 @@
+package ru.innopolis.university.homework.lesson2.task1;
+
+class NullPointerException {
+    public static void main(String... args) {
+        Object fake = null;
+        fake.toString();
+    }
+}
