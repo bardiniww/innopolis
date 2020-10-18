841766d8a6ae681ad3b25f6ad2100ec891038038
Ivan Bardin <bardiniww@gmail.com>
2020-10-18 12:16:23 +0300

    javadocs added

--- src/main/java/ru/innopolis/university/homework/lesson2/task1/CustomException.java
+++ src/main/java/ru/innopolis/university/homework/lesson2/task1/CustomException.java
@@ -1,6 +1,12 @@
 package ru.innopolis.university.homework.lesson2.task1;
 
 class CustomException {
+
+    /**
+     * Lesson 2, task 1.
+     *
+     * @throws java.lang.StackOverflowError
+     */
     public static void main(String... args) {
         req();
     }
--- src/main/java/ru/innopolis/university/homework/lesson2/task1/NullPointerException.java
+++ src/main/java/ru/innopolis/university/homework/lesson2/task1/NullPointerException.java
@@ -1,6 +1,12 @@
 package ru.innopolis.university.homework.lesson2.task1;
 
 class NullPointerException {
+
+    /**
+     * Lesson 2, task 1.
+     *
+     * @throws java.lang.NullPointerException
+     */
     public static void main(String... args) {
         Object fake = null;
         fake.toString();
